acfc8ef5df99ab380f723e21824e9e9ac4a68e54
Hossein Samani <samanihossein1379@gmail.com>
2021-12-14 22:26:42 +0330

    Header component added to site

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <Login />
   </div>
 </template>
 
 <script>
 import Login from "./components/Login.vue";
+import Header from "./components/Header.vue";
 export default {
   name: "App",
   components: {
     Login,
+    Header,
   },
 };
 </script>
