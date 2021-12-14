68261b673cbd9001512068071c70145b1a1bbe37
Hossein Samani <samanihossein1379@gmail.com>
2021-12-14 19:01:06 +0330

    directory component added

---
+++ src/components/directory/Directory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="directory-wrapper">
+    <Breadcrump />
+    <div>
+      <span class="material-icons driver"> drive_folder_upload </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import Breadcrump from "../breadcrump";
+export default {
+  name: "directory-component",
+  components: {
+    Breadcrump,
+  },
+};
+</script>
+
+<style scoped>
+.directory-wrapper {
+  width: 70%;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.driver {
+  font-size: 200px;
+}
+</style>
