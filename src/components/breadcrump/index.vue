1842b19eb0215d63c4b5890a047aa6bd6860ea8a
Hossein Samani <samanihossein1379@gmail.com>
2021-12-14 19:00:46 +0330

    breadcrump component added

---
+++ src/components/breadcrump/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="breadcrump-wrapper">
+    <span class="material-icons"> home </span>
+    <span>Home</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "breadcrump-component",
+};
+</script>
+
+<style>
+.breadcrump-wrapper {
+  display: flex;
+  align-items: center;
+}
+</style>
