0b5913163adf2e7479ff3f2e0a3c7294f06ce8a1
Hossein Samani <samanihossein1379@gmail.com>
2021-12-14 22:43:12 +0330

    jwt and user name added to header componnt

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,37 @@
 <template>
   <header>
     <div>
-      <Button title="Logout"/>
-      <span>name</span>
+      <div @change.stop="deleteToken()">
+        <Button title="Logout" />
+      </div>
+      <span>{{ userEmail }}</span>
     </div>
   </header>
 </template>
 
 <script>
 import Button from "./btn/Button.vue";
+import { deleteToken } from "../services/jwt";
 export default {
   name: "header-component",
   components: {
     Button,
   },
+  computed: {
+    userEmail() {
+      const email = localStorage.getItem("email");
+      if (email) return email;
+      else return null;
+    },
+  },
+  methods: {
+    deleteToken,
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+span {
+  margin-left: 10px;
+}
+</style>
