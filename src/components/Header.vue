aa9dd92c10486cf7c61493e09dab5bdfbb22c182
Hossein Samani <samanihossein1379@gmail.com>
2021-12-14 22:26:49 +0330

    Header component added to site

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div>
+      <Button title="Logout"/>
+      <span>name</span>
+    </div>
+  </header>
+</template>
+
+<script>
+import Button from "./btn/Button.vue";
+export default {
+  name: "header-component",
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style></style>
