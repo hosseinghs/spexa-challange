cff8f8c3858f0296b163bd7c406220764a5f5040
Hossein Samani <samanihossein1379@gmail.com>
2021-12-14 10:37:10 +0330

    button and input component improved

---
+++ src/components/btn/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="btn-wrapper">
+    <button :type="type">{{ title }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "button-component",
+  props: {
+    title: {
+      type: String,
+      default: "btn title",
+    },
+    type: {
+      type: String,
+      default: "btn type",
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  background-color: #000;
+  color: #fff;
+  transition: background-color 0.1s ease-in;
+}
+button:hover {
+  background-color: #555;
+}
+</style>
---
+++ src/components/form/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="input-wrapper">
+    <input :type="type" :placeholder="placeholder" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "input-component",
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    placeholder: {
+      type: String,
+      default: "placeholder",
+    },
+  },
+};
+</script>
+
+<style></style>
