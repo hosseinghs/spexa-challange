5cd4b2d7d48ee7474d8e79e1c06f4868664f5a1b
Hossein Samani <samanihossein1379@gmail.com>
2021-12-16 01:25:58 +0330

    app fixed a little bit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <!-- <Header /> -->
-    <Login />
+    <Header v-if="isLoggedIn" />
+    <Login v-if="!isLoggedIn" />
+    <Directory v-if="isLoggedIn" />
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from "vuex";
+import { getToken } from "./services/jwt";
 import Login from "./components/Login.vue";
-// import Header from "./components/Header.vue";
+import Header from "./components/Header.vue";
+import Directory from "./components/directory/Directory.vue";
 export default {
   name: "App",
   components: {
     Login,
-    // Header,
+    Header,
+    Directory,
+  },
+  created() {
+    const hasToken = getToken();
+    if (hasToken) this.setLoginState(!!hasToken);
+  },
+  computed: {
+    ...mapGetters("loginRegister", ["isLoggedIn"]),
+  },
+  methods: {
+    getToken,
+    ...mapActions("loginRegister", ["setLoginState"]),
   },
 };
 </script>
