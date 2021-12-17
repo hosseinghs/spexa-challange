01d3f5d763c334af98d2883b47cdc103b722a967
Hossein Samani <samanihossein1379@gmail.com>
2021-12-17 18:10:12 +0330

    directoryListItem component ui improved

--- src/components/directory/DirectoryListItem.vue
+++ src/components/directory/DirectoryListItem.vue
@@ -1,27 +1,44 @@
 <template>
   <div class="directory-list-item-wrapper">
     <div class="i">
-      <span @click="showOptions">...</span>
+      <span class="more" @click="showOptions">...</span>
       <img src="../../assets/img/folder.png" alt="more-icon" />
+      <span>{{ directoryName }}</span>
     </div>
+    <div>+</div>
   </div>
 </template>
 
 <script>
 export default {
   name: "directory-list-item-component",
+  props: {
+    directoryName: {
+      type: String,
+      default: "directoryName",
+    },
+  },
 };
 </script>
 
 <style scoped>
-.i {
-  position: relative;
+.directory-list-item-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: start;
 }
 
-.i span {
+.i {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.i .more {
   position: absolute;
-  top: -10px;
-  right: 5px;
+  top: 0px;
+  right: 12px;
   font-size: 18px;
   font-weight: bold;
   cursor: pointer;
