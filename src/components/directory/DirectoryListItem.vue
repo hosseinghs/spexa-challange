4df2c20d2c8562df76af69c0778e4f886613b374
Hossein Samani <samanihossein1379@gmail.com>
2021-12-17 18:23:33 +0330

    add image added in directoryListItem

--- src/components/directory/DirectoryListItem.vue
+++ src/components/directory/DirectoryListItem.vue
@@ -5,17 +5,26 @@
       <img src="../../assets/img/folder.png" alt="more-icon" />
       <span>{{ directoryName }}</span>
     </div>
-    <div>+</div>
+    <div @click="setLoginState(true)">
+      <img src="../../assets/img/add.png" alt="add" />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "directory-list-item-component",
   props: {
     directoryName: {
       type: String,
       default: "directoryName",
+    },
+  },
+  methods: {
+    ...mapActions("modal", ["setLoginState"]),
+    showOptions() {
+      console.log("show options");
     },
   },
 };
@@ -26,6 +35,10 @@ export default {
   display: flex;
   align-items: center;
   justify-content: start;
+}
+
+.directory-list-item-wrapper div:last-child {
+  cursor: pointer;
 }
 
 .i {
