5ff5957f7153db9cad9143251476907c7bdef307
Hossein Samani <samanihossein1379@gmail.com>
2021-12-18 00:39:52 +0330

    dropdown component added

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          style="background: transparent"
+          elevation="0"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="{ title, id } in items" :key="id">
+          <v-list-item-title>{{ title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
