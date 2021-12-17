ccce68f74bc2b34c0613178e233c5f038cb19669
Hossein Samani <samanihossein1379@gmail.com>
2021-12-17 10:50:32 +0330

    modal component implemented and added to direcory component

---
+++ src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="modal-wrapper" @click.self="setLoginState(false)">
+    <div class="modal-content">hi</div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+export default {
+  name: "modal-component",
+  computed: {
+    ...mapState("modal", ["visible"]),
+  },
+  methods: {
+    ...mapActions("modal", ["setLoginState"]),
+  },
+};
+</script>
+
+<style scoped>
+.modal-wrapper {
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  place-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  background: rgba(#fff, 0.4);
+  z-index: 10;
+}
+.modal-wrapper > div {
+  width: 100%;
+  z-index: 20;
+}
+
+.modal-content {
+  background-color: #f2f2f2;
+  text-align: left;
+  border-radius: 8px;
+  padding: 1rem;
+}
+</style>
--- src/components/directory/Directory.vue
+++ src/components/directory/Directory.vue
@@ -4,19 +4,28 @@
     <div>
       <span class="material-icons driver"> drive_folder_upload </span>
       <h3>There is nothing to show</h3>
-      <Button title="Create directory" />
+      <div @click.stop="setLoginState(true)">
+        <Button title="Create directory" />
+      </div>
     </div>
+    <Modal />
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 import Breadcrump from "../breadcrump";
 import Button from "../btn/Button.vue";
+import Modal from "../Modal.vue";
 export default {
   name: "directory-component",
   components: {
     Breadcrump,
     Button,
+    Modal,
+  },
+  methods: {
+    ...mapActions("modal", ["setLoginState"]),
   },
 };
 </script>
