55de0b16d9056e40fc54e9a825c29e5555817a21
Hossein Samani <samanihossein1379@gmail.com>
2021-12-18 01:23:50 +0330

    clearDirectoryTitle action added

--- src/store/modules/directory.js
+++ src/store/modules/directory.js
@@ -3,15 +3,14 @@ import {
   getDirectoryContentApi,
   deleteDirectoryApi,
 } from "../../services/directory";
-
 import { addToArr } from "../../utils/general";
-
+import { Directory } from "../../models/directory";
 export const directory = {
   namespaced: true,
   state: {
     visible: false,
     newDirectoryTitle: null,
-    directories: [],
+    directories: new Directory(),
   },
   mutations: {
     SET_NEW_DIRECTORY_TITLE(state, title) {
@@ -22,14 +21,21 @@ export const directory = {
       arr.splice(0);
       addToArr(arr, payload);
     },
+    CLEAR_DIRECTORY_TITLE(state) {
+      state.newDirectoryTitle = "";
+    },
   },
   actions: {
     setNewDirectoryTitle({ commit }, title) {
       commit("SET_NEW_DIRECTORY_TITLE", title);
     },
-    async createNewDirectory({ state }) {
+    clearDirectoryTitle({ commit }) {
+      commit("CLEAR_DIRECTORY_TITLE");
+    },
+    async createNewDirectory({ state, dispatch }) {
       const title = state.newDirectoryTitle;
       await createNewDirectoryApi(title);
+      dispatch("getDirectoryContent");
     },
     async getDirectoryContent({ commit }) {
       const res = await getDirectoryContentApi();
@@ -43,5 +49,6 @@ export const directory = {
   },
   getters: {
     directories: (state) => state.directories,
+    title: (state) => state.newDirectoryTitle,
   },
 };
