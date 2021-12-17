334c820ae6374e7e4e9999053b2286c277c96d5b
Hossein Samani <samanihossein1379@gmail.com>
2021-12-17 18:23:16 +0330

    items bind to DirectoryList component

--- src/components/directory/DirectoryList.vue
+++ src/components/directory/DirectoryList.vue
@@ -1,10 +1,22 @@
 <template>
-  <div><DirectoyListItem /></div>
+  <div>
+    <DirectoyListItem
+      v-for="{ id, title } in items"
+      :directoryName="title"
+      :key="id"
+    />
+  </div>
 </template>
 
 <script>
 import DirectoyListItem from "./DirectoryListItem.vue";
 export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
   components: {
     DirectoyListItem,
   },
