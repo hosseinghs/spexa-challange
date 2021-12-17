b08c999dddd31a028c5821fb9100bc392a3d5ef4
Hossein Samani <samanihossein1379@gmail.com>
2021-12-17 21:18:51 +0330

    vuetify added to project

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import Api from "./services/api";
-import "./assets/main.css";
+import VueRouter from "vue-router";
+import vuetify from "./plugins/vuetify";
+
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
 Api.init();
 new Vue({
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
