3668007b422367d4099e1ea64c3d7e365c3e11eb
Hossein Samani <samanihossein1379@gmail.com>
2021-12-17 21:26:26 +0330

    routes intregated with app

--- src/main.js
+++ src/main.js
@@ -4,12 +4,13 @@ import store from "./store";
 import Api from "./services/api";
 import VueRouter from "vue-router";
 import vuetify from "./plugins/vuetify";
-
+import routes from "./router";
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 Api.init();
 new Vue({
+  routes,
   store,
   vuetify,
   render: (h) => h(App),
