48172733998bcf8ee8a5fcb55be60d11c0d1e6d4
Hossein Samani <samanihossein1379@gmail.com>
2021-12-17 10:49:59 +0330

    modal module added to store

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,13 @@ import Vuex from "vuex";
 import { createStore } from "vuex-extensions";
 
 Vue.use(Vuex);
+
 import { loginRegister } from "./modules/login";
+import { modal } from "./modules/modal";
+
 export default createStore(Vuex.Store, {
   modules: {
     loginRegister,
+    modal,
   },
 });
