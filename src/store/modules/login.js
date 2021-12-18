ebaf5866cec66a495740fd9bad878382eae1b221
Hossein Samani <samanihossein1379@gmail.com>
2021-12-18 15:31:55 +0330

    loginRegister bug fixed

--- src/store/modules/login.js
+++ src/store/modules/login.js
@@ -28,10 +28,12 @@ export const loginRegister = {
     },
     async loginRegisterUser({ state, commit }) {
       const { email, password } = state.userData;
-      await loginRegisterApi({ email, password });
-      commit("SET_LOGIN_STATE", true);
-      window.localStorage.setItem("email", email);
-      return true;
+      const res = await loginRegisterApi({ email, password });
+      if (res.status === 200) {
+        commit("SET_LOGIN_STATE", true);
+        window.localStorage.setItem("email", email);
+        return true;
+      } else return false;
     },
   },
   getters: {
