907abd245feecc6a7d8e1ae9076b182e0f2ce2de
Hossein Samani <samanihossein1379@gmail.com>
2021-12-18 12:27:56 +0330

    snackbar componnt added

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-snackbar v-model="snackbarVisible" :timeout="timeout">
+    {{ text }}
+    <template v-slot:action="{ attrs }">
+      <v-btn color="blue" text v-bind="attrs" @click="snackbar = false">
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "text",
+    },
+    timeout: {
+      type: Number,
+      default: 3000,
+    },
+  },
+  computed: {
+    ...mapState("snackbar", ["visible"]),
+    snackbarVisible: {
+      get() {
+        return this.visible;
+      },
+      set(val) {
+        this.setSnackbarState(val);
+      },
+    },
+  },
+  methods: {
+    ...mapActions("snackbar", ["setSnackbarState"]),
+  },
+};
+</script>
+
+<style></style>
